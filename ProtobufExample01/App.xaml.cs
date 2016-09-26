ee9857b0b71642ab5b3d256171cb8c79387a39ca
Anton Bakulev <abakulev@hotmail.com>
2016-09-26 13:33:16 +0300

    Initial commit. MvvmLight wizard project 451

---
+++ ProtobufExample01/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using GalaSoft.MvvmLight.Threading;
+
+namespace ProtobufExample01
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        static App()
+        {
+            DispatcherHelper.Initialize();
+        }
+    }
+}
